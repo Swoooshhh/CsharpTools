42abcbf1205045e9608aaf8a2135898b20eb5dd0
Swoooshhh <57326660+Swoooshhh@users.noreply.github.com>
2021-02-15 11:10:03 +1000

    Add files via upload

---
+++ IpfinderCSHARP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            files data = new files();
+            string filelocation = "C:\\Users\\Swoos\\Documents\\c.txt";
+            do
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1: Find someone, 2: Add someone to the list, 3: Exit : ");
+                string response = Console.ReadLine();
+                if (response == "1")
+                {
+                    Console.WriteLine("Who would you like to find? : ");
+                    string name = Console.ReadLine();
+                    data.filefind(filelocation, name);
+                }
+                else if (response == "2")
+                {
+                    Console.WriteLine("What's the name of the person you'd like to add? : ");
+                    string name2add = Console.ReadLine();
+                    Console.WriteLine("What's their address? : ");
+                    string address = Console.ReadLine();
+
+                    data.filewrite(filelocation, name2add, address);
+                }
+                else if (response == "3")
+                {
+                    Console.WriteLine("Bye!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Choice. Choose 1-3");
+                }
+
+            } while (true);
+
+        }
+    }
+}
---
+++ IpfinderCSHARP/files.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Learn
+{
+    class files
+    {
+        public string filefind(string file, string name)
+        {
+            FileStream fsSource = new FileStream(file, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fsSource);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+                if (line.Contains(name))
+                {
+                    Console.WriteLine(line);
+                }
+            
+            sr.Close();
+            fsSource.Close();
+            return line;
+        }
+
+        public void filewrite(string file, string name, string address)
+        {
+            FileStream fappend = File.Open(file, FileMode.Append);
+
+            StreamWriter x = new StreamWriter(fappend);
+            string full = $"{name} {address}";
+            x.WriteLineAsync(full);
+
+            x.Close();
+
+        }
+    }
+}
